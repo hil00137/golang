43e980b5ae62dafb91edecb3827014caff29ec84
hil00137 <hil00137@gmail.com>
2024-08-29 10:14:19 +0900

    프로그래머스 181952

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+// golang은 go.mod 에 선언되어있는 module name
+import "golang/programmers"
+
+func main() {
+	programmers.Sol181952()
+}
---
+++ programmers/programmers.go
@@ -0,0 +1,12 @@
+package programmers
+
+import "fmt"
+
+// Sol181952 대문자로 시작할경우 Public 함수
+func Sol181952() {
+	var s1 string
+	// 입력된 값을 s1에 할당
+	fmt.Scan(&s1)
+	// 출력
+	fmt.Println(s1)
+}
